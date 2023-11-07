c5ea8d0ead296c4b54c9b4e12355f2fc87eb3733
Samuel Quaresma <squoliver@gmail.com>
2023-11-07 11:49:39 -0300

    EventController tests OK

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -7,8 +7,11 @@ import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.CityRepository;
 import com.devsuperior.demo.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,8 +21,22 @@ public class EventService {
     @Autowired
     private EventRepository repository;
 
-    public List<EventDTO> findAll() {
-        List<Event> list = repository.findAll();
-        return list.stream().map(x -> new EventDTO(x)).toList();
+    @Autowired
+    private CityRepository cityRepository;
+
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> list = repository.findAll(pageable);
+        return list.map(x -> new EventDTO(x));
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(cityRepository.getReferenceById(dto.getCityId()));
+        repository.save(entity);
+        return new EventDTO(entity);
     }
 }
